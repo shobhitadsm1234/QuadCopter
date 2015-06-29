5da7af1f6da4565b0bd0cdb080b5cde8124615f7
Spencer Schmalz <schmalz.spencer@gmail.com>
2015-06-29 15:04:10 -0500

    Cleaned up code and added comments

--- MPU6050/ReadValues/ReadValues.ino
+++ MPU6050/ReadValues/ReadValues.ino
@@ -1,97 +1,79 @@
+//NOTE: This code requires I2Cdev and MPU6050 libaries found here: https://github.com/jrowberg/i2cdevlib
 #include "I2Cdev.h"
 #include "MPU6050.h"
+#include "Wire.h"
 
+MPU6050 accelgyro;  //Create new MPU6050 object called accelgyro
 
-#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-  #include "Wire.h"
-#endif
-
-MPU6050 accelgyro;
-
+//Define variables to store gyroscope and accelerometer data
 int16_t ax, ay, az;
 int16_t gx, gy, gz;
   
 void setup() {
-  #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-    Wire.begin();
-  #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
-    Fastwire::setup)(400, true);
-  #endif
-  
-  
+  Wire.begin();
   Serial.begin(38400);
+  
+  //Initialize MPU6050 and check to see if it connected successfully
   accelgyro.initialize();
   Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
-  
-  //accelgyro.setXGyroOffset(4061);
 }
 
-float angle_pitch;
-float angle_roll;
-float timeBeginPitch = millis();
-float timeBeginRoll = millis();
-float pitch;
-float roll;
+//Define variables
+float angle_pitch, angle_roll, timeBeginPitch = millis(), timeBeginRoll = millis(), pitch, roll;
 
 void loop() {
-  accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-  gx = (gx/131)-31;
+  accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);  //Get raw data values
+  gx = (gx/131)-31;  //Divide gyroscope data by 131 to get degrees per second (-31 is an offset for this specific module)
   gy = gy/131;
   gz = gz/131;
-  /*Serial.print("a/g:\t");
-  Serial.print((float)ax/16384); Serial.print("\t");
-  Serial.print((float)ay/16384); Serial.print("\t");
-  Serial.print((float)az/16384); Serial.print("\t");*/
-  //Serial.print(gx); Serial.print("\t");
-  //Serial.print(gy); Serial.print("\t");
-  //Serial.print(gz); Serial.print("\t");
-  //Serial.print(pitch); Serial.print("\t");
-  //Serial.println(roll);
-  calc_xy_angles((float)ax/16384, (float)ay/16384, (float)az/16384);
-  Serial.print(comp_filter_roll(roll, gz)); Serial.print("\t");
-  Serial.println(comp_filter_pitch(pitch, gy));
+  
+  calc_xy_angles((float)ax/16384, (float)ay/16384, (float)az/16384);  //Calculate angles from accelerometer
+  
+  //Print out pitch and roll
+  Serial.print(comp_filter_pitch(pitch, gy)); Serial.print("\t");
+  Serial.println(comp_filter_roll(roll, gz));
+  
   delay(20);
 }
 
 
+//Function to calculate angles based off of accelerometer
 void calc_xy_angles(float accel_value_x, float accel_value_y, float accel_value_z){
-   // Using x y and z from accelerometer, calculate x and y angles
-   float x_val, y_val, z_val, result;
-   float accel_center_x = .05;
-   float accel_center_y = -.02;
-   float accel_center_z = -.06;
-   unsigned long x2, y2, z2; //24 bit
-
-   // Lets get the deviations from our baseline
-   x_val = (float)accel_value_x-(float)accel_center_x;
-   y_val = (float)accel_value_y-(float)accel_center_y;
-   z_val = (float)accel_value_z-(float)accel_center_z;
+  
+  float x_val, y_val, z_val, result;
+  float accel_center_x = .05, accel_center_y = -.02, accel_center_z = -.06;  //Offsets to zero acceleromter data
+  unsigned long x2, y2, z2;
    
-   roll = atan(y_val/sqrt(pow(x_val,2) + pow(z_val,2)));
-   pitch = atan(z_val/sqrt(pow(y_val,2) + pow(x_val,2))); 
-   
-   pitch = pitch * (180.0/PI);
-   roll = roll * (180.0/PI) ;
-
-   //Serial.print(pitch); Serial.print("\t");
-   //Serial.println(roll);
+  //Find value after removing offset
+  x_val = (float)accel_value_x-(float)accel_center_x;
+  y_val = (float)accel_value_y-(float)accel_center_y;
+  z_val = (float)accel_value_z-(float)accel_center_z;
+  
+  //Use trigonometry to calculate angles from accelerations
+  roll = atan(y_val/sqrt(pow(x_val,2) + pow(z_val,2)));
+  pitch = atan(z_val/sqrt(pow(y_val,2) + pow(x_val,2))); 
+  
+  //Convert angles from radians to degrees
+  pitch = pitch * (180.0/PI);
+  roll = roll * (180.0/PI) ;
 }
 
 
+//Functions to combine gyroscope and acceleromter data
 float comp_filter_pitch(float accel, float gyro) {
-  float dt = ((timeBeginPitch - millis())/1000) * -1;
-  timeBeginPitch = millis();
+  float dt = ((timeBeginPitch - millis())/1000) * -1; //Calculate time since last run
+  timeBeginPitch = millis(); //Reset last run time
   
-  angle_pitch = (0.98) * (angle_pitch + (gyro * dt)) + (.02) * accel;
+  angle_pitch = (0.98) * (angle_pitch + (gyro * dt)) + (.02) * accel; //Combine gyro and accelerometer data using complimentary filter
   
-  return angle_pitch;
+  return angle_pitch; //Return value
 }
 
 float comp_filter_roll(float accel, float gyro) {
-  float dt = ((timeBeginRoll - millis())/1000);
-  timeBeginRoll = millis();
+  float dt = ((timeBeginRoll - millis())/1000); //Calculate time since last run
+  timeBeginRoll = millis(); //Reset last run time
   
-  angle_roll = (0.98) * (angle_roll + (gyro * dt)) + (.02) * accel;
+  angle_roll = (0.98) * (angle_roll + (gyro * dt)) + (.02) * accel; //Combine gyro and acceleromter data using complimntary filter
   
-  return angle_roll;
+  return angle_roll; //Return value
 }
